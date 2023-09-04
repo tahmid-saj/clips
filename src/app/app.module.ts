99419f64c953e6049259a5816a9afff7b9c21902
tahmid-saj <tahmid.saj@gmail.com>
2023-09-04 13:34:57 -0400

    adding user module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { UserModule } from "./user/user.module";
 import { AppComponent } from './app.component';
 
 @NgModule({
