2bcf4518b2fee5aef2f174ed2a8520193a4cb1bd
tahmid-saj <tahmid.saj@gmail.com>
2023-09-04 18:12:56 -0400

    changing active tab

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -13,6 +13,20 @@ export class TabsContainerComponent implements AfterContentInit {
   }
 
   ngAfterContentInit(): void {
-      this.tabs
+    const activeTabs = this.tabs?.filter(
+      tab => tab.active
+    )
+
+    if (!activeTabs || activeTabs.length === 0) {
+      this.selectTab(this.tabs!.first)
+    }
+  }
+  
+  selectTab(tab: TabComponent) {
+    this.tabs?.forEach(tab => {
+      tab.active = false
+    })
+  
+    tab.active = true;
   }
 }
