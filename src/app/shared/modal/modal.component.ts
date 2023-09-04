6efe496b039f7f890bc59fd727742e49bc956930
tahmid-saj <tahmid.saj@gmail.com>
2023-09-04 14:43:51 -0400

    modal service

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-modal',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./modal.component.css']
 })
 export class ModalComponent {
-
+  constructor(public modal: ModalService) {
+    console.log(this.modal.visible);
+  }
 }
