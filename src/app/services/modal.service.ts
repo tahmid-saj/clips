eaa0a63a30d0775402eaceb191dc5b8f7ddcde02
tahmid-saj <tahmid.saj@gmail.com>
2023-09-04 16:22:09 -0400

    ID system for modals

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -1,18 +1,32 @@
 import { Injectable } from '@angular/core';
 
+interface IModal {
+  id: string,
+  visible: boolean
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class ModalService {
-  private visible = false
+  private modals: IModal[] = [];
 
-  constructor() { }
+  constructor() {
+
+  }
+
+  register(id: string) {
+    this.modals.push({
+      id,
+      visible: false
+    })
+  }
 
   isModalOpen() {
-    return this.visible;
+    return true;
   }
 
   toggleModal() {
-    this.visible = !this.visible;
+    // this.visible = !this.visible;
   }
 }
--- src/app/user/auth-modal/auth-modal.component.ts
+++ src/app/user/auth-modal/auth-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-auth-modal',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./auth-modal.component.css']
 })
 export class AuthModalComponent {
+  constructor(public modal: ModalService) {
 
+  }
+
+  ngOnInit(): void {
+    this.modal.register("auth");
+    this.modal.register("test");
+  }
 }
