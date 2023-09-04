8c5b24aad59ccaf5d68b228dd9440172f00fccf4
tahmid-saj <tahmid.saj@gmail.com>
2023-09-04 17:40:48 -0400

    content children

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, AfterContentInit, ContentChildren } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
 
 @Component({
   selector: 'app-tabs-container',
   templateUrl: './tabs-container.component.html',
   styleUrls: ['./tabs-container.component.css']
 })
-export class TabsContainerComponent {
+export class TabsContainerComponent implements AfterContentInit {
+  @ContentChildren(TabComponent) tabs = {}
+  constructor() {
 
+  }
+
+  ngAfterContentInit(): void {
+      
+  }
 }
