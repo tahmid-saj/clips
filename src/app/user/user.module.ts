dd40a60ecb84f39dba057f47e9ee899de9dca5ae
tahmid-saj <tahmid.saj@gmail.com>
2023-09-07 22:40:19 -0400

    reactive forms

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { SharedModule } from '../shared/shared.module';
 import { LoginComponent } from './login/login.component';
@@ -16,6 +17,7 @@ import { RegisterComponent } from './register/register.component';
   imports: [
     CommonModule,
     SharedModule,
+    ReactiveFormsModule
   ],
   exports: [
     AuthModalComponent
