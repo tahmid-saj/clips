bbd4fbfd8d2844414e3a45278d47061370ff49ba
tahmid-saj <tahmid.saj@gmail.com>
2023-09-08 03:13:32 -0400

    applying input masking

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -28,7 +28,11 @@ export class RegisterComponent {
   confirm_password = new FormControl("", [
     Validators.required
   ])
-  phoneNumber = new FormControl("")
+  phoneNumber = new FormControl("", [
+    Validators.required,
+    Validators.minLength(13),
+    Validators.maxLength(13)
+  ])
 
   registerForm = new FormGroup({
     name: this.name,
